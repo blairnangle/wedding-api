9251fd399e6630b47703223d5a78506842ccdfa2
Blair Nangle <hi@blairnangle.com>
2022-07-21 12:58:06 +0100

    Remove now-redundant Terraform code

--- infra/secrets_manager.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_secretsmanager_secret" "google_sheets_api_client_secret" {
-  name = "google_sheets_api_client_secret"
-}
-
-resource "aws_secretsmanager_secret_version" "google_sheets_api_client_secret_initial" {
-  secret_id     = aws_secretsmanager_secret.google_sheets_api_client_secret.id
-  secret_string = file("../client_secret.json")
-}
