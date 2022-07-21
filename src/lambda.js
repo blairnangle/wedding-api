fe3559045fce3d228cc101a7995b1d03f29009ee
Blair Nangle <hi@blairnangle.com>
2022-07-21 14:57:59 +0100

    Again

--- src/lambda.js
+++ src/lambda.js
@@ -36,16 +36,16 @@ if (process.env.ENV === 'local') {
   });
 } else {
   try {
-    const googleSheetsClientSecret = await fetchJSONSecret('google-sheets-client-secret');
-    console.log('googleSheetsClientSecret', googleSheetsClientSecret);
-    const googleSheetID = await fetchJSONSecret('google-sheet-id').then((jsonSecret) => jsonSecret.value);
-    console.log('googleSheetsClientSecret', googleSheetsClientSecret);
-    app.set('googleSheetsClientSecret', googleSheetsClientSecret);
-    app.set('googleSheetID', googleSheetID);
     const handler = serverless(app, {
       binary: binaryMimeTypes,
     });
     module.exports.lambda = async (context, req) => {
+      const googleSheetsClientSecret = await fetchJSONSecret('google-sheets-client-secret');
+      console.log('googleSheetsClientSecret', googleSheetsClientSecret);
+      const googleSheetID = await fetchJSONSecret('google-sheet-id').then((jsonSecret) => jsonSecret.value);
+      console.log('googleSheetsClientSecret', googleSheetsClientSecret);
+      app.set('googleSheetsClientSecret', googleSheetsClientSecret);
+      app.set('googleSheetID', googleSheetID);
       context.res = await handler(context, req);
     }
   } catch (error) {
